1dac7d2ecf983d1b3fb64ad40c2a22ee38bdc417
unknown <hrkamizato@gmail.com>
2021-01-09 03:11:47 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+        </footer>
+
+    )
+}
+
+export default Footer;
